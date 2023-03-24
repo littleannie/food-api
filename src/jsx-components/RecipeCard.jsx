c0aa01dc0c0db2c49b53725395e1ebd88c35408f
Anne Ly <anne.ly59@gmail.com>
2023-03-24 11:52:42 +0100

    edit recipeCard design

--- src/jsx-components/RecipeCard.jsx
+++ src/jsx-components/RecipeCard.jsx
@@ -40,13 +40,12 @@ const RecipeCard = ({ recipe }) => {
         {modal && (
           <div className="modal">
             <div className="overlay"></div>
-            <div className="modal-content">
-              <h2>Hello </h2>
-              <p>coucou</p>
-              <button className="close-modal"
-                    onClick={toggleModal}>
-                CLOSE
-              </button>
+            <div className="modal-content flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={image} alt={title}></img>
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">A compléter.</p>
+                </div>
             </div>
           </div>
         ) }
